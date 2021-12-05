e6326f4a1a46dc9f8c5546ad8d57dc45b55bae2e
alexmuniz96 <alexmuniz096@gmail.com>
2021-12-04 23:17:01 -0300

    add components Sidebar and Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "./MovieCard"
+
+interface ContentProps {
+  genresTitle: {
+    title: string;
+  };
+  movieProperty: [
+    {
+      imdbID: string;
+      Title: string;
+      Poster: string;
+      Ratings: Array<{
+        Source: string;
+        Value: string;
+      }>;
+      Runtime: string;
+    }
+  ]
+}
+export function Content({ genresTitle, movieProperty }: ContentProps) {
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {genresTitle.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movieProperty.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
